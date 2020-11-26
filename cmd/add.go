a1247a71fb2bb429272f928842d0ecb25acedbd6
Will <will7989@hotmail.com>
2020-11-26 21:32:23 +0000

    added searching and printing results

--- cmd/add.go
+++ cmd/add.go
@@ -10,29 +10,29 @@ import (
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "Add a new bookmar,",
+	Short: "Add a new bookmark",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return saveBookmark(args)
 	},
 }
 
 var bookmark string
+var url string
 var searchTerms []string
 
 func init() {
 	rootCmd.AddCommand(addCmd)
 
-	addCmd.Flags().StringVar(&bookmark, "url", "", "Help message for toggle")
-	addCmd.Flags().StringSliceVar(&searchTerms, "terms", []string{}, `pass in any search terms you wish to use, seperated by a comma. Example: --terms="first term" "second"`)
+	addCmd.Flags().StringVar(&url, "url", "", "the url of the bookmark")
+	addCmd.Flags().StringVar(&bookmark, "name", "", "the name of the bookmark")
+	addCmd.Flags().StringSliceVar(&searchTerms, "terms", []string{}, `pass in any search terms you wish to use, seperated by a comma. Example: --terms="first term, second"`)
 }
 
 func saveBookmark(args []string) error {
-	store, err := pkg.NewStore(args)
+	store, err := pkg.NewStore()
 	if err != nil {
 		return errors.Wrap(err, "failed to create store")
 	}
 
-	store.SaveBookmark(bookmark, searchTerms)
-
-	return nil
+	return store.SaveBookmark(bookmark, url, searchTerms)
 }
--- pkg/store.go
+++ pkg/store.go
@@ -1,46 +1,127 @@
 package pkg
 
 import (
-	"errors"
-	"flag"
+	"context"
 	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/pkg/errors"
 
 	"github.com/pacedotdev/firesearch-sdk/clients/go/firesearch"
 )
 
+const (
+	endpoint  = "http://localhost:8888/api"
+	secret    = "firesearch-playground"
+	indexPath = "bookmarks"
+)
+
 type Store struct {
 	autocompleteService *firesearch.AutocompleteService
+	indexPath           string
 }
 
-func NewStore(args []string) (*Store, error) {
-	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
-	var (
-		endpoint  = flags.String("endpoint", "http://localhost:8888/api", "Firesearch API endpoint")
-		secret    = flags.String("secret", "firesearch-playground", "Secret API key")
-		indexPath = flags.String("index", "firesearch-tutorial/indexes/movies-index", "Firesearch index path")
-	)
-	if err := flags.Parse(args[1:]); err != nil {
-		return nil, err
-	}
-	if *endpoint == "" {
-		return nil, errors.New("missing endpoint")
-	}
-	if *indexPath == "" {
-		return nil, errors.New("missing indexPath")
-	}
+func NewStore() (*Store, error) {
 
-	client := firesearch.NewClient(*endpoint, *secret)
+	client := firesearch.NewClient(endpoint, secret)
 	autocompleteService := firesearch.NewAutocompleteService(client)
+	createIndex(context.TODO(), autocompleteService, indexPath)
 
 	return &Store{
 		autocompleteService: autocompleteService,
+		indexPath:           indexPath,
 	}, nil
 }
 
-func (s *Store) SaveBookmark(url string, searchTerms []string) {
-	for _, st := range searchTerms {
-		fmt.Println(st)
+func createIndex(ctx context.Context, service *firesearch.AutocompleteService, index string) error {
+	createAutocompleteIndexReq := firesearch.CreateAutocompleteIndexRequest{
+		Index: firesearch.AutocompleteIndex{
+			IndexPath:     index,
+			Name:          "Bookmark autocomplete index",
+			CaseSensitive: false,
+		},
+	}
+	_, err := service.CreateIndex(ctx, createAutocompleteIndexReq)
+	if err != nil {
+		return errors.Wrap(err, "firesearch: IndexService.CreateIndex")
 	}
 
-	fmt.Printf("URL: %s\n", url)
+	return nil
+}
+
+func (s *Store) SaveBookmark(bookmarkName, url string, searchTerms []string) error {
+
+	// if the bookmark name isn't in the search terms, add it in so it can be searched for
+	if checkIfBookmarkNameIsInSearchTerms(searchTerms, bookmarkName) == false {
+		searchTerms = append(searchTerms, bookmarkName)
+	}
+
+	err := s.addDocument(context.TODO(), bookmarkName, url, searchTerms)
+	if err != nil {
+		return errors.Wrap(err, "error storing bookmark document")
+	}
+
+	return nil
+}
+
+func checkIfBookmarkNameIsInSearchTerms(searchTerms []string, bookmarkName string) bool {
+	for _, item := range searchTerms {
+		if item == bookmarkName {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Store) addDocument(ctx context.Context, bookmarkName, url string, searchTerms []string) error {
+	_, err := s.autocompleteService.PutDoc(ctx, firesearch.PutAutocompleteDocRequest{
+		IndexPath: s.indexPath,
+		Doc: firesearch.AutocompleteDoc{
+			ID:   bookmarkName,
+			Text: strings.Join(searchTerms, " "),
+			Fields: []firesearch.Field{
+				{
+					Key:   "url",
+					Value: url,
+				},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) FindBookmark(searchTerm string) error {
+	return s.getDocuments(context.TODO(), searchTerm)
+}
+
+func (s *Store) getDocuments(ctx context.Context, searchTerm string) error {
+	res, err := s.autocompleteService.Complete(ctx, firesearch.CompleteRequest{
+		Query: firesearch.CompleteQuery{
+			IndexPath: s.indexPath,
+			Text:      searchTerm,
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	defer w.Flush()
+
+	fmt.Fprintln(w, "Name\tURL\t")
+	fmt.Fprintln(w, "\t")
+	for _, hit := range res.Hits {
+		fmt.Fprintf(w, "%s\t%s\n", hit.ID, hit.Fields[0].Value)
+	}
+
+	fmt.Fprintln(w)
+	return nil
 }
